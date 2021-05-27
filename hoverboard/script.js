2e20348a1060d035fdc4005b6e35e6ba2c1c0612
JohnCMello <johncharlesmello@gmail.com>
2021-05-27 14:42:41 -0300

    hoverboard

---
+++ hoverboard/script.js
@@ -0,0 +1,36 @@
+const container = document.getElementById('container');
+const colors = [
+  '#f9c80e',
+  '#f86624',
+  '#ea3546',
+  '#662e9b',
+  '#43bccd',
+];
+
+const SQUARES = 700
+
+for (i = 0; i < SQUARES; i++) {
+  const square = document.createElement('div')
+  square.classList.add('square')
+
+  square.addEventListener('mouseover', () => setColor(square))
+  square.addEventListener('mouseout', () => removeColor(square))
+
+  container.appendChild(square)
+}
+
+function setColor(element) {
+  const color = getRandomColor()
+
+  element.style.background = color;
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+  element.style.background = '#1b263b'
+  element.style.boxShadow = '0 0 2px #000'
+}
+
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)]
+}
