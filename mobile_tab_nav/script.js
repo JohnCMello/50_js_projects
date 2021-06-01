42780188db04923694b8e8d0c2ac37735011bf7d
JohnCMello <johncharlesmello@gmail.com>
2021-06-01 15:15:30 -0300

    change bg on item :hover .active

--- mobile_tab_nav/script.js
+++ mobile_tab_nav/script.js
@@ -1,5 +1,5 @@
 const contents = document.querySelectorAll('.content')
-const listItems = document.querySelectorAll('nav ul li')
+const listItems = document.querySelectorAll('.item')
 
 listItems.forEach((item, i) => {
   item.addEventListener('click', () => {
